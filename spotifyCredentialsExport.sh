ba07230dbc58537e7ef32d78da26227cc2b0f901
Patrick S. Watters <pwatters@ups.com>
2017-12-23 17:35:13 -0500

    Update spotifyCredentialsExport.sh

--- spotifyCredentialsExport.sh
+++ spotifyCredentialsExport.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export SPOTIPY_CLIENT_ID=59dacdf9fdb4423080b5958d4670e545
 export SPOTIPY_CLIENT_SECRET=32cc3540e3f44ed6bb9d544b4c85490b
 export SPOTIPY_REDIRECT_URI=http://localhost:8888/callback
