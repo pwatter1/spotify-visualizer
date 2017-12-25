699d82064d6d00a5dd437af53a64b4b7e5150c2b
pwatter1 <pwatter1@binghamton.edu>
2017-12-25 16:19:08 -0500

    adding credit

--- visualizer.py
+++ visualizer.py
@@ -1,32 +1,37 @@
-import struct
-import pyaudio
-import sys
+"""
+Credit to MarkJay4k's visualizer tutorial. This is sweet.
+"""
+
 import numpy as np
 from opensimplex import OpenSimplex
 import pyqtgraph.opengl as gl
 from pyqtgraph.Qt import QtCore, QtGui
+import struct
+import pyaudio
+import sys
 
 
 class Terrain(object):
-
     def __init__(self):
+        """
+        Initialize the graphics window and mesh surface
+        """
         # setup the view window
         self.app = QtGui.QApplication(sys.argv)
         self.window = gl.GLViewWidget()
-        self.window.setWindowTitle('Spotify Visualizer')
+        self.window.setWindowTitle('Terrain')
         self.window.setGeometry(0, 110, 1920, 1080)
-        self.window.resize(1000, 600)
-        self.window.setCameraPosition(distance=60, elevation=12)
+        self.window.setCameraPosition(distance=30, elevation=12)
         self.window.show()
 
-        # constants/arrays
+        # constants and arrays
         self.nsteps = 1.3
         self.offset = 0
         self.ypoints = np.arange(-20, 20 + self.nsteps, self.nsteps)
         self.xpoints = np.arange(-20, 20 + self.nsteps, self.nsteps)
         self.nfaces = len(self.ypoints)
 
-        self.RATE = 44100 # standard 4.1 khz
+        self.RATE = 44100
         self.CHUNK = len(self.xpoints) * len(self.ypoints)
 
         self.p = pyaudio.PyAudio()
@@ -42,6 +47,7 @@ class Terrain(object):
         # perlin noise object
         self.noise = OpenSimplex()
 
+        # create the veritices array
         verts, faces, colors = self.mesh()
 
         self.mesh1 = gl.GLMeshItem(
@@ -53,7 +59,6 @@ class Terrain(object):
         )
         self.mesh1.setGLOptions('additive')
         self.window.addItem(self.mesh1)
-
 
     def mesh(self, offset=0, height=2.5, wf_data=None):
 
@@ -69,45 +74,59 @@ class Terrain(object):
 
         faces = []
         colors = []
-
-        # [x, y, wf_data[xid][yid] * (1.5 * self.noise.noise2d(x=xid / 5 + offset, y=yid / 5 + offset))] 
-
-
         verts = np.array([
-            [x, y, wf_data[xid][yid] * (1.5 * self.noise.noise2d(x=(xid/5), y=(yid/5)))] 
-			for xid, x in enumerate(self.xpoints) for yid, y in enumerate(self.ypoints)
+            [
+                x, y, wf_data[xid][yid] * self.noise.noise2d(x=xid / 5 + offset, y=yid / 5 + offset)
+            ] for xid, x in enumerate(self.xpoints) for yid, y in enumerate(self.ypoints)
         ], dtype=np.float32)
 
         for yid in range(self.nfaces - 1):
             yoff = yid * self.nfaces
             for xid in range(self.nfaces - 1):
-                faces.append([xid + yoff, xid + yoff + self.nfaces, xid + yoff + self.nfaces + 1])
-                faces.append([xid + yoff, xid + yoff + 1, xid + yoff + self.nfaces + 1])
-                colors.append([xid / self.nfaces, 1 - xid / self.nfaces, yid / self.nfaces, 0.1])
-                colors.append([xid / self.nfaces, 1 - xid / self.nfaces, yid / self.nfaces, 0.11])
+                faces.append([
+                    xid + yoff,
+                    xid + yoff + self.nfaces,
+                    xid + yoff + self.nfaces + 1,
+                ])
+                faces.append([
+                    xid + yoff,
+                    xid + yoff + 1,
+                    xid + yoff + self.nfaces + 1,
+                ])
+                colors.append([
+                    xid / self.nfaces, 1 - xid / self.nfaces, yid / self.nfaces, 0.7
+                ])
+                colors.append([
+                    xid / self.nfaces, 1 - xid / self.nfaces, yid / self.nfaces, 0.8
+                ])
 
         faces = np.array(faces, dtype=np.uint32)
         colors = np.array(colors, dtype=np.float32)
 
         return verts, faces, colors
 
-
     def update(self):
-        # update the mesh and shift the noise each time
+        """
+        update the mesh and shift the noise each time
+        """
+
         wf_data = self.stream.read(self.CHUNK)
+
         verts, faces, colors = self.mesh(offset=self.offset, wf_data=wf_data)
         self.mesh1.setMeshData(vertexes=verts, faces=faces, faceColors=colors)
         self.offset -= 0.05
 
-
     def start(self):
-        # open graphics window and set it up
+        """
+        get the graphics window open and setup
+        """
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
             QtGui.QApplication.instance().exec_()
 
-
     def animation(self, frametime=10):
-        # calls update method to run in a loop
+        """
+        calls the update method to run in a loop
+        """
         timer = QtCore.QTimer()
         timer.timeout.connect(self.update)
         timer.start(frametime)
@@ -116,4 +135,5 @@ class Terrain(object):
 
 if __name__ == '__main__':
     t = Terrain()
-    t.animation()
+	t.animation()
+
